a40b2fd7c680f0b909b1cf485db6fb76aa01b844
Erik Källén <erik_kallen@hotmail.com>
2012-11-07 16:25:31 +0100

    Some minor fixes

--- KockoutJS/Official Samples/OfficialSamplesWeb/Global.asax.cs
+++ KockoutJS/Official Samples/OfficialSamplesWeb/Global.asax.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
